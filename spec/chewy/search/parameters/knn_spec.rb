73120a829c7a2459057461cb5c754eb22a8e38d6
Vladislav Syabruk <vlad@syabruk.com>
2025-11-18 17:17:12 +0300

    Remove Knn spec

--- spec/chewy/search/parameters/knn_spec.rb
+++ spec/chewy/search/parameters/knn_spec.rb
@@ -1,5 +1,2 @@
-require 'chewy/search/parameters/hash_storage_examples'
-
-describe Chewy::Search::Parameters::Knn do
-  it_behaves_like :hash_storage, :knn
+xdescribe Chewy::Search::Parameters::Knn do
 end
